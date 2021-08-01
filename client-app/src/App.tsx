e9fdd9eb0acf3c78b1d4beca9312699b7e44fe73
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-08-01 15:17:02 +0700

    add semectic ui to react app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Header } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
+      <Header as='h1' icon='users' content="hahaha" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
