a54e5a940602cf8e9d8ff9048550bf5ac33bf2d8
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-08-02 23:30:03 +0700

    add crud funtion to react ui

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,7 @@
 using Application.Activities;
 using Application.Core;
 using AutoMapper;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
---
+++ client-app/src/app/layout/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+interface Props {
+    openForm: () => void
+}
+
+const NavBar: React.FC<Props> = ({ openForm }) => {
+
+    return (
+        <Menu inverted fixed='top' >
+            <Container>
+                <Menu.Item header>
+                    <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
+                    Reactivities
+                </Menu.Item>
+
+                <Menu.Item name='Activities' />
+
+                <Menu.Item>
+                    <Button onClick={openForm} positive content='Create Activity' />
+                </Menu.Item>
+            </Container>
+
+        </Menu>
+    )
+}
+
+export default NavBar;
