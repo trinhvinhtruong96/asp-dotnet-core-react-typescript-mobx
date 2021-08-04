018614190cc712e6ae3935f0180f69be1b40b9bd
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-08-04 23:48:20 +0700

    style page and seprate to small component

---
+++ client-app/src/features/activities/dashboard/ActivityFilters.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+import { Header, Menu } from 'semantic-ui-react';
+
+export interface ActivityFilterProps {
+
+}
+
+const ActivityFilter: React.FC<ActivityFilterProps> = () => {
+    return (
+        <>
+            <Menu vertical size='large' style={{ width: '100%', marginTop: 25 }} >
+                <Header icon="filter" attached color='teal' content='Filters' />
+                <Menu.Item content="All Activities" />
+                <Menu.Item content="I'm going" />
+                <Menu.Item content="I'm hosting" />
+            </Menu>
+            <Header />
+            <Calendar />
+        </>
+    );
+}
+
+export default ActivityFilter;
---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,50 @@
+import React, {  } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Icon, Item, Segment } from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+
+interface ActivityListItemProps {
+    activity: Activity
+}
+
+const ActivityListItem: React.FC<ActivityListItemProps> = ({ activity }) => {
+
+    return (
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' src='/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`} >
+                                {activity.title}
+                            </Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
+    );
+}
+
+export default ActivityListItem;
