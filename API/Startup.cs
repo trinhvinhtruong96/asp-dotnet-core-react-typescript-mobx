184de852858f4a50f4b3854d7f4a3c5d7b102f48
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2021-08-06 00:02:54 +0700

    add validation and handler error

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using API.Extensions;
 using Application.Activities;
 using Application.Core;
 using AutoMapper;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -34,7 +35,11 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services
+            .AddControllers()
+            .AddFluentValidation(config => {
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddApplicationServices(_config);
 
         }
